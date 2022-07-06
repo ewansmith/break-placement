2bf0d65a74b0172fb4d26c367e192fcb048d4b94
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2022-07-06 15:24:57 +0100

    round to 2%

--- utilities.py
+++ utilities.py
@@ -9,3 +9,38 @@ def convertProdID(id):
     new = partJoined.replace('#', '.')
 
     return new
+
+def roundPartial(x, base=0.02):
+    '''
+    Return x rounded to 5
+    '''
+    return base * round(x / base)
+
+
+def getMidpoint(start, end):
+    '''
+    Return the integer exactly halfway between start and end
+    '''
+    return (start + end) / 2
+
+def timecodeToFrames(timecode):
+    '''
+    Return timecode in number of frames
+    '''
+    temp = timecode.split(':')
+    answer = [int(x) for x in temp]
+
+    hours = (answer[0]-10) * (3600 * 25)
+    mins = answer[1] * 60 * 25
+    secs = (answer[2] * 25)
+    return hours + mins + secs + answer[3]
+
+def count_elements(seq):
+    """
+    Tally elements from `seq' and return dictionary
+    """
+    dict = {}
+    for i in seq:
+        dict[i] = dict.get(i, 0) + 1
+    return dict
+
